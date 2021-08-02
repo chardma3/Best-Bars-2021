4688ed84de6db76149057706a2dcd14297aadf2a
chardma3 <chardma3@hotmail.com>
2021-08-02 16:24:27 +0000

    removed border from container fluid elements on index

--- app.py
+++ app.py
@@ -162,15 +162,15 @@ def page_not_found(e):
 
 
 @app.errorhandler(500) 
+# 500 error handle
 def page_not_found(e): 
-# 500 error handler
-return render_template('500.html'), 500
+    return render_template('500.html'), 500
 
 
-@app.errorhandler(403) 
+@app.errorhandler(403)
+# 403 error handler 
 def page_not_found(e): 
-# 403 error handler
-return render_template('403.html'), 403
+    return render_template('403.html'), 403
 
 
 if __name__ == "__main__":
